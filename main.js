f468e45a3648f990d27d7d3acd0c0fc954ad0bc3
Cesar Nsingi <cesarricardo1805@hotmail.com>
2024-07-16 08:45:09 +0400

    hours of sleep function

---
+++ main.js
@@ -0,0 +1,34 @@
+const getSleepHours = day => {
+  
+    switch(day) {
+      case 'monday':
+      return 8
+      break;
+  
+      case 'tuesday':
+      return 7
+      break;
+  
+      case 'wednesday':
+      return 8
+      break;
+  
+      case 'thursday':
+      return 5
+      break;
+  
+      case 'friday':
+      return 8
+      break;
+  
+      case 'saturday':
+      return 7
+      break;
+  
+      case 'sunday':
+      return 8
+      break;
+      default:
+      return "Error"
+    }
+  }; // how many hours of sleep function
